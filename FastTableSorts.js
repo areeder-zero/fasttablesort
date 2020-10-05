3bd08ad0d9a703d467c0420b2345ef0589aa7bc5
LubuZero <57199354+areeder-zero@users.noreply.github.com>
2020-10-05 08:22:39 -0500

    Update FastTableSorts.js
    Added additional loop to group on next element (si/xi)

--- FastTableSorts.js
+++ FastTableSorts.js
@@ -1,4 +1,4 @@
-function revisedSortTable(){
+function revisedSortTable() {
     var table, rows, switching, i, j, x, y, ai, aj, bi, bj, si, sj, xi, xj, shouldSwitch;
     table = document.getElementById("totable");
     switching = true;
@@ -19,23 +19,42 @@ function revisedSortTable(){
             switching = true;
         }
     }
-	for (i = 1; i < (rows.length - 1); i++) {
+    for (i = 1; i < (rows.length - 1); i++) {
         for (j = i + 1; j < (rows.length); j++) {
             ai = rows[i].getElementsByTagName("TD")[11];
             aj = rows[j].getElementsByTagName("TD")[11];
             bi = rows[i].getElementsByTagName("TD")[12];
             bj = rows[j].getElementsByTagName("TD")[12];
-			si = rows[i].getElementsByTagName("TD")[13];
-			sj = rows[j].getElementsByTagName("TD")[13];
-			xi = rows[i].getElementsByTagName("TD")[14];
-			xj = rows[j].getElementsByTagName("TD")[14];
+            si = rows[i].getElementsByTagName("TD")[13];
+            sj = rows[j].getElementsByTagName("TD")[13];
+            xi = rows[i].getElementsByTagName("TD")[14];
+            xj = rows[j].getElementsByTagName("TD")[14];
             if (Number(ai.innerHTML) == Number(aj.innerHTML) && isOdd(Number(ai.innerHTML)) == true) {
                 if (Number(bi.innerHTML) > Number(bj.innerHTML)) {
                     rows[i].parentNode.insertBefore(rows[j], rows[i]);
                 }
             }
-          	if (Number(ai.innerHTML) == Number(aj.innerHTML) && isOdd(Number(ai.innerHTML)) == false) {
+            if (Number(ai.innerHTML) == Number(aj.innerHTML) && isOdd(Number(ai.innerHTML)) == false) {
                 if (Number(bi.innerHTML) < Number(bj.innerHTML)) {
+                    rows[i].parentNode.insertBefore(rows[j], rows[i]);
+
+                }
+            }
+        }
+    }
+    for (i = 1; i < (rows.length - 1); i++) {
+        for (j = i + 1; j < (rows.length); j++) {
+            si = rows[i].getElementsByTagName("TD")[13];
+            sj = rows[j].getElementsByTagName("TD")[13];
+            xi = rows[i].getElementsByTagName("TD")[14];
+            xj = rows[j].getElementsByTagName("TD")[14];
+            if (Number(si.innerHTML) == Number(sj.innerHTML)) {
+                if (Number(xi.innerHTML) > Number(xj.innerHTML)) {
+                    rows[i].parentNode.insertBefore(rows[j], rows[i]);
+                }
+            }
+            if (Number(si.innerHTML) == Number(sj.innerHTML)) {
+                if (Number(xi.innerHTML) < Number(xj.innerHTML)) {
                     rows[i].parentNode.insertBefore(rows[j], rows[i]);
 
                 }
